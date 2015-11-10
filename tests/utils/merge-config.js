6b9b663a83fa8d522ded4b043766281fbc5eeada
Mikael Lepistö <mikael.lepisto@vincit.fi>
2015-11-10 14:56:03 +0200

    Merge config to support $featureId and more consistent $remove support

--- tests/utils/merge-config.js
+++ tests/utils/merge-config.js
@@ -38,7 +38,18 @@ describe('merge config', function () {
           someConstructor: SomeConstructor,
           array: [1, 2, 3]
         }
+      },
+      {
+        $featureId: 'feature 3 instance 1',
+        feature: 'feature 3',
+        config: { }
+      },
+      {
+        $featureId: 'feature 3 instance 2',
+        feature: 'feature 3',
+        config: { }
       }
+
     ]
   };
 
@@ -101,19 +112,43 @@ describe('merge config', function () {
 
   it('$remove in feature definition should remove the feature', function () {
     var feature =         {
-      $remove: true,
-      feature: 'feature 2'
+      $remove: 'feature 2'
     };
 
     var merged = mergeConfig(baseConfig, {
       features: [feature]
     });
 
-    expect(merged.features).to.eql([baseConfig.features[0]]);
+    expect(merged.features).to.eql([baseConfig.features[0], baseConfig.features[2], baseConfig.features[3]]);
   });
 
-  it('$addBefore in feature definition should add the feature before the given feature', function () {
+  it('$remove in feature definition should remove the feature with unique id reference', function () {
     var feature =         {
+      $remove: 'feature 3 instance 2'
+    };
+
+    var merged = mergeConfig(baseConfig, {
+      features: [feature]
+    });
+
+    expect(merged.features).to.eql([baseConfig.features[0], baseConfig.features[1], baseConfig.features[2]]);
+  });
+
+  it('$remove should fail if no unique id given', function () {
+    var feature = {
+      $remove: 'feature 3'
+    };
+
+    expect(function () {
+      mergeConfig(baseConfig, {
+        features: [feature]
+      });
+    }).to.throw();
+  });
+
+
+  it('$addBefore in feature definition should add the feature before the given feature', function () {
+    var feature = {
       $addBefore: 'feature 2',
       feature: 'add-before-test',
       config: {
@@ -125,7 +160,39 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.eql([baseConfig.features[0], feature, baseConfig.features[1]]);
+    expect(merged.features).to.eql([baseConfig.features[0], feature, baseConfig.features[1], baseConfig.features[2], baseConfig.features[3]]);
+  });
+
+  it('$addBefore in feature should allow $featureId reference', function () {
+    var feature = {
+      $addBefore: 'feature 3 instance 2',
+      feature: 'add-before-test',
+      config: {
+        something: 'here'
+      }
+    };
+
+    var merged = mergeConfig(baseConfig, {
+      features: [feature]
+    });
+
+    expect(merged.features).to.eql([baseConfig.features[0], baseConfig.features[1], baseConfig.features[2], feature, baseConfig.features[3]]);
+  });
+
+  it('$addBefore should throw without unique feature reference', function () {
+    var feature = {
+      $addBefore: 'feature 3',
+      feature: 'add-before-test',
+      config: {
+        something: 'here'
+      }
+    };
+
+    expect(function () {
+      mergeConfig(baseConfig, {
+        features: [feature]
+      });
+    }).to.throw();
   });
 
   it('$addAfter in feature definition should add the feature after the given feature', function () {
@@ -141,7 +208,7 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.eql([baseConfig.features[0], feature, baseConfig.features[1]]);
+    expect(merged.features).to.eql([baseConfig.features[0], feature, baseConfig.features[1], baseConfig.features[2], baseConfig.features[3]]);
 
     feature = {
       $addAfter: 'feature 2',
@@ -155,7 +222,45 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.eql([baseConfig.features[0], baseConfig.features[1], feature]);
+    expect(merged.features).to.eql([baseConfig.features[0], baseConfig.features[1], feature, baseConfig.features[2], baseConfig.features[3]]);
+  });
+
+  it('$addAfter should work with $featureId reference', function () {
+    var feature = {
+      $addAfter: 'feature 3 instance 2',
+      feature: 'add-after-test',
+      config: {
+        something: 'here'
+      }
+    };
+
+    var merged = mergeConfig(baseConfig, {
+      features: [feature]
+    });
+
+    expect(merged.features).to.eql([
+      baseConfig.features[0],
+      baseConfig.features[1],
+      baseConfig.features[2],
+      baseConfig.features[3],
+      feature
+    ]);
+  });
+
+  it('$addAfter should throw without unique feature reference', function () {
+    var feature = {
+      $addAfter: 'feature 3',
+      feature: 'add-after-test',
+      config: {
+        something: 'here'
+      }
+    };
+
+    expect(function () {
+      mergeConfig(baseConfig, {
+        features: [feature]
+      });
+    }).to.throw();
   });
 
   it('feature definition should be merged with an existing one', function () {
@@ -176,7 +281,7 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.have.length(2);
+    expect(merged.features).to.have.length(4);
     expect(merged.features[1]).to.eql(baseConfig.features[1]);
     expect(merged.features[0]).to.eql({
       feature: 'feature 1',
@@ -193,6 +298,27 @@ describe('merge config', function () {
     });
   });
 
+  it('merge should throw if no unique feature reference', function () {
+    var feature = {
+      feature: 'feature 3',
+      config: {
+        f: 'morjens',
+        a: {
+          c: 666,
+          d: {
+            e: 'jeah'
+          }
+        }
+      }
+    };
+    expect(function () {
+      mergeConfig(baseConfig, {
+        features: [feature]
+      });
+    }).to.throw();
+  });
+
+
   it('function should not be merged', function () {
     function NewConstructor() {}
     NewConstructor.newStaticProperty = 'wuppi';
@@ -208,7 +334,7 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.have.length(2);
+    expect(merged.features).to.have.length(4);
     expect(merged.features[0]).to.eql(baseConfig.features[0]);
     expect(merged.features[1]).to.eql({
       feature: 'feature 2',
@@ -241,10 +367,10 @@ describe('merge config', function () {
       features: [feature]
     });
 
-    expect(merged.features).to.have.length(3);
+    expect(merged.features).to.have.length(5);
     expect(merged.features[0]).to.eql(baseConfig.features[0]);
     expect(merged.features[1]).to.eql(baseConfig.features[1]);
-    expect(merged.features[2]).to.eql(feature);
+    expect(merged.features[4]).to.eql(feature);
   });
 
 });
