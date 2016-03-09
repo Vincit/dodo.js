472f2aa7e23852d0c7a8dee26ed13bd163e6dd91
Mikael Lepistö <mikael.lepisto@vincit.fi>
2016-03-09 21:02:34 +0200

    Create app and start server tests

--- tests/app/main.spec.js
+++ tests/app/main.spec.js
@@ -2,10 +2,13 @@
 
 var expect = require('chai').expect;
 var main = require('../../lib/app/express/main');
+var request = require('../../lib/http/request');
 
 var path = require('path');
 var featurePath1 = path.join(__dirname, 'feature-path1');
 var featurePath2 = path.join(__dirname, 'feature-path2');
+
+var _ = require('lodash');
 
 describe('main.js app create / start', function () {
   var okConfig = null;
@@ -19,6 +22,9 @@ describe('main.js app create / start', function () {
           config: {
             featureConfigParameter: true
           }
+        },
+        {
+          feature : 'single-file-feature'
         }
       ],
       featurePaths: [ featurePath1, featurePath2 ],
@@ -51,36 +57,111 @@ describe('main.js app create / start', function () {
 
     it('should fail if starting twice at same port', function () {
       okConfig.port = 45932;
+      // TODO: actually we should also close the started server
       return Promise.all([
         main.start(okConfig),
         main.start(okConfig)
       ]).then(function () {
         throw new Error("Should have failed in start() and pass error to catch!");
       }).catch(function (err) {
-        expect(err.message).to.equal('listen EADDRINUSE :::45932');
+        expect(err.message).to.contain('listen EADDRINUSE');
       });
-    });
-
-    it('should have req.app added to request got from express', function () {
     });
   });
 
   describe('createApp(app)', function () {
     it('should init features', function () {
+      var app = main.createApp(okConfig);
+      expect(app.feature('feature-in-path')).to.be.ok;
     });
 
     it('should emit appReady event after features are initialized', function () {
+      var app = main.createApp(okConfig);
+      var events = app.feature('feature-in-path').events;
+      var firstAppReadyEvent = _(events).pluck('event').indexOf('appReady');
+      var lastFeatureInitEvent = _(events).pluck('event').lastIndexOf('featureInitializeEnd');
+      expect(firstAppReadyEvent > lastFeatureInitEvent, JSON.stringify(events,null,2)).to.be.ok;
+    });
+
+    it('should have req.app added to request got from express', function () {
+      okConfig.port = 45933;
+      var app = main.createApp(okConfig);
+      app.get('/', function (req, res) {
+        res.sendStatus(req.app ? 200 : 400);
+      });
+
+      return main.startApp(app)
+        .then(function () {
+          return request('http://localhost:' + okConfig.port)
+            .get('/')
+            .then(function (res) {
+              expect(res.statusCode).to.equal(200);
+            });
+        })
+        .finally(function () {
+          app.server.close();
+        });
     });
   });
 
   describe('startServer(app)', function () {
-    it('should create http server', function () {
+    it('should throw an error if config.protocol == https and no ssl config given', function () {
+      okConfig.protocol = 'https';
+      var app = main.createApp(okConfig);
+      return new Promise(function (resolve, reject) {
+        // fail promise if start app is success and success if starting is fail
+        main.startApp(app).then(reject).catch(resolve);
+      }).then(function (err) {
+        expect(err.message).to.contain("Https requires ssl configuration with key and cert");
+      });
     });
 
-    it('should create https server if config.protocol == https', function () {
+    it('should be able to create https server with key, cert and passphrase', function () {
+      okConfig.protocol = 'https';
+      okConfig.ssl = {
+        key: path.join(__dirname, 'data', 'server.key'),
+        cert: path.join(__dirname, 'data', 'server.crt'),
+        passphrase: 'dodo'
+      };
+      var app = main.createApp(okConfig);
+      return main.startApp(app)
+        .then(function () {
+          app.server.close();
+        });
+    });
+
+    it('should be able to create https server with uncrypted key and cert', function () {
+      okConfig.protocol = 'https';
+      okConfig.ssl = {
+        key: path.join(__dirname, 'data', 'server.unencrypted.key'),
+        cert: path.join(__dirname, 'data', 'server.crt')
+      };
+      var app = main.createApp(okConfig);
+      return main.startApp(app)
+        .then(function () {
+          app.server.close();
+        });
+    });
+
+    it('should fail if crypted key and cert given without passphrase', function () {
+      okConfig.protocol = 'https';
+      okConfig.ssl = {
+        key: path.join(__dirname, 'data', 'server.key'),
+        cert: path.join(__dirname, 'data', 'server.crt')
+      };
+      var app = main.createApp(okConfig);
+      return new Promise(function (resolve, reject) {
+        // fail promise if start app is success and success if starting is fail
+        main.startApp(app).then(reject).catch(resolve);
+      });
     });
 
     it('should emit serverStart event after server is started', function () {
+      var app = main.createApp(okConfig);
+      return new Promise(function (resolve, reject) {
+        app.on('serverStart', resolve);
+        main.startApp(app);
+      });
     });
   });
 
--- tests/app/util/mock-feature-generator.js
+++ tests/app/util/mock-feature-generator.js
@@ -1,8 +1,36 @@
 'use strict';
 
+var featureInitializationEvents = [];
+
 module.exports = function (featureId, dependencies) {
   var generatedFeature = function (app, config) {
+    // reset global feature collecting when initializing first feature of an app
+    if (!app.features) {
+      featureInitializationEvents.splice(0, featureInitializationEvents.length);
+    }
+
+    var events = this.events = featureInitializationEvents;
+    events.push({
+      featureId: featureId,
+      event: 'featureInitializeStart'
+    });
+
     this.featureId = featureId;
+
+    // log events for testing
+    if (app.on) {
+      app.on('appReady', function (app) {
+        events.push({featureId: featureId, event: 'appReady'});
+      });
+      app.on('serverStart', function (app) {
+        events.push({featureId: featureId, event: 'serverStart'});
+      });
+    }
+
+    events.push({
+      featureId: featureId,
+      event: 'featureInitializeEnd'
+    });
   };
   generatedFeature.dependencies = dependencies || [];
   return generatedFeature;
